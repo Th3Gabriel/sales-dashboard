5929c672f7d83b7e4e5b173b8e83b960f69c3a55
Gabriel Martins <gabrielmartins1955@hotmail.com>
2024-11-29 10:24:58 -0300

    Feat: Adding Styled Components

---
+++ src/pages/Leads.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+function Leads() {
+  return (
+    <>
+      <h1>Leads</h1>
+    </>
+  )
+}
+
+export default Leads
---
+++ src/pages/Profile.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+function Profile() {
+  return (
+    <>
+      <h1>Profile</h1>
+    </>
+  )
+}
+
+export default Profile
