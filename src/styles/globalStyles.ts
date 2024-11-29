e242468947d78474cee4b82f989b237671361edc
Gabriel Martins <gabrielmartins1955@hotmail.com>
2024-11-29 16:42:57 -0300

    feat: Create Themes

---
+++ src/styles/globalStyles.ts
@@ -0,0 +1,22 @@
+import { createGlobalStyle } from 'styled-components'
+import { DefaultTheme } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle<{ theme?: DefaultTheme }>`
+* {
+margin: 0;
+padding: 0;
+font-family: 'Inter', sans-serif;
+}
+
+body, html {
+background:${(props) => props.theme.appBackground};
+color:${(props) => props.theme.appColor};
+margin: 0;
+padding: 0;
+}
+
+h1, h2, h3, h4, h5, h6, p {
+margin: 0;
+padding: 0;
+}
+`
