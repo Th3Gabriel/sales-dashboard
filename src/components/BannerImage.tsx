13d77334552f4f3157045404e5ad45c68497d04a
Gabriel Martins <gabrielmartins1955@hotmail.com>
2024-11-29 17:58:59 -0300

    Feat material-ui-insert

---
+++ src/components/BannerImage.tsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const BannerImage = styled.div`
+  background-image: url(./login-image.svg);
+  background-size: cover;
+  height: 100vh;
+  width: 50vh;
+`
+
+export default BannerImage
