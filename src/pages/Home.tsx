866aaa782bf750e380e60e06908062f15c9b9ce5
Gabriel Martins <gabrielmartins1955@hotmail.com>
2024-12-04 22:08:44 -0300

    Feat: avatar list

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { Header, CardComponent } from '@/components'
+import { Header, CardComponent, AvatarsList } from '@/components'
+import { currencyConverter } from '@/utils'
 import { Container } from '@mui/material'
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: '/avatar.svg',
+      name: 'Nome Sobrenome 1',
+      subtitle: currencyConverter(1234.54),
+    },
+    {
+      avatar: '/avatar.svg',
+      name: 'Nome Sobrenome 2',
+      subtitle: currencyConverter(3234.54),
+    },
+    {
+      avatar: '/avatar.svg',
+      name: 'Nome Sobrenome 3',
+      subtitle: currencyConverter(3421.54),
+    },
+  ]
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <CardComponent className="success">Card</CardComponent>
+        <CardComponent>Card</CardComponent>
+        <CardComponent>
+          <AvatarsList listData={mockListData} />
+        </CardComponent>
       </Container>
       <h1>Home</h1>
     </>
